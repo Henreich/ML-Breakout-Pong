542cfbd44b462190fb29a187ad8fa7d0452c21c2
Henrik <lordminning@gmail.com>
2018-10-18 16:06:45 +0200

    Committing at the end of the day
    Work in progress. Currently working on getting the paddle to act somewhat intelligently.
    Started fleshing out the logic for when to give rewards and when to give punishments, but
    game mechanic problems are currently in the way for further advancements.
    Read more on the gitlab issue.

--- UnitySDK/Assets/ML-Agents/Examples/Pong/Scripts/Ball.cs
+++ UnitySDK/Assets/ML-Agents/Examples/Pong/Scripts/Ball.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour {
-    public float speed = 5f;
+    public float speed = 2f;
     public int lastHitBy = 0;
+    public PaddleAgent agent;
 
     //private PaddleAgent agentA;
     //private PaddleAgent agentB;
@@ -18,6 +19,8 @@ public class Ball : MonoBehaviour {
         float sy = Random.Range(0, 2) == 0 ? -1 : 1;
 
         GetComponent<Rigidbody>().velocity = new Vector3(speed * sx, speed * sy, 0f);
+
+        //agent = agent.GetComponent<PaddleAgent>();
     }
 	
 	// Update is called once per frame
@@ -27,31 +30,62 @@ public class Ball : MonoBehaviour {
 
 
     private void OnCollisionEnter(Collision collision)
-    {   
-        // TODO: Make a switch case instead?
+    {
 
-        // Who hit the ball last, specificlly only whenever a collision
-        // with a paddle happens
-        if(collision.gameObject.name == "Paddle1")
+        //Debug.Log(collision.gameObject.name);
+        //Remember who touched the ball last.
+        if (collision.gameObject.name == "PaddleAgent")
         {
             lastHitBy = 1;
         }
-        else if(collision.gameObject.name == "Paddle2")
+        else if (collision.gameObject.name == "Paddle2")
         {
             lastHitBy = 2;
-        } else if(collision.gameObject.name == "Wall2" && lastHitBy == 1)
+        }
+
+        if (collision.gameObject.name == "Wall1")
         {
-            scorePlayer1++;
+            agent.AddReward(-1f);
+        }
+        else if (collision.gameObject.name == "Wall2" && lastHitBy == 1)
+        {
+            agent.AddReward(0.5f);
+            agent.score++;
             TextMesh Score1 = GameObject.Find("Score1").GetComponent<TextMesh>();
-            Score1.text = "Score: " + scorePlayer1;
+            Score1.text = "Score: " + agent.score;
         }
-        else if (collision.gameObject.name == "Wall1" && lastHitBy == 2)
-        {
-            scorePlayer2++;
-            TextMesh Score2 = GameObject.Find("Score2").GetComponent<TextMesh>();
-            Score2.text = "Score: " + scorePlayer2;
-        }
-        TextMesh textObject = GameObject.Find("LastHitBy").GetComponent<TextMesh>();
-        textObject.text = "Last hit by: " + lastHitBy.ToString();
+        agent.Done();
+
+
+
+
+
+
+
+        //// TODO: Make a switch case instead?
+
+        //// Who hit the ball last, specificlly only whenever a collision
+        //// with a paddle happens
+        //if(collision.gameObject.name == "Paddle1")
+        //{
+        //    lastHitBy = 1;
+        //}
+        //else if(collision.gameObject.name == "Paddle2")
+        //{
+        //    lastHitBy = 2;
+        //} else if(collision.gameObject.name == "Wall2" && lastHitBy == 1)
+        //{
+        //    scorePlayer1++;
+        //TextMesh Score1 = GameObject.Find("Score1").GetComponent<TextMesh>();
+        //Score1.text = "Score: " + scorePlayer1;
+        //}
+        //else if (collision.gameObject.name == "Wall1" && lastHitBy == 2)
+        //{
+        //    scorePlayer2++;
+        //    TextMesh Score2 = GameObject.Find("Score2").GetComponent<TextMesh>();
+        //    Score2.text = "Score: " + scorePlayer2;
+        //}
+        //TextMesh textObject = GameObject.Find("LastHitBy").GetComponent<TextMesh>();
+        //textObject.text = "Last hit by: " + lastHitBy.ToString();
     }
 }
--- UnitySDK/Assets/ML-Agents/Examples/Pong/Scripts/PaddleAgent.cs
+++ UnitySDK/Assets/ML-Agents/Examples/Pong/Scripts/PaddleAgent.cs
@@ -5,7 +5,10 @@ using MLAgents;
 
 public class PaddleAgent : Agent {
     public GameObject ball;
-    public GameObject opponent;
+    public float speed = 2f;
+    public int score = 0;
+
+    //public GameObject opponent;
 
     private Rigidbody agentRb;
     private Rigidbody ballRb;
@@ -19,14 +22,27 @@ public class PaddleAgent : Agent {
     public override void CollectObservations()
     {
         // Relative position of the ball compared to paddle (?)
-        AddVectorObs(transform.position.x - ball.transform.position.x);
-        AddVectorObs(transform.position.y - ball.transform.position.y);
+        AddVectorObs(this.transform.position.x - ball.transform.position.x);
+        AddVectorObs(this.transform.position.y - ball.transform.position.y);
 
-        // Observe the direction and speed of the agent.
+        // Observe the velocity of the agent.
         AddVectorObs(agentRb.velocity.y);
 
         // Observe the direction and speed of the ball.
         AddVectorObs(ballRb.velocity.x);
         AddVectorObs(ballRb.velocity.y);
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        Debug.Log(vectorAction[0]);
+        //Debug.Log(vectorAction[1]);
+        //agentRb.velocity = new Vector3(0f, vectorAction[0], 0f);
+        transform.Translate(0f, vectorAction[0] * speed * Time.deltaTime, 0f);
+        //switch (vectorAction)
+        //{
+
+        //}
+
     }
 }
